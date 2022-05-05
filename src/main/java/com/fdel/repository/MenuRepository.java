28aa32ae6fd7239006570aa51c10f2fba8e78f8e
wookieOH <ehddnrdh@gmail.com>
2022-05-05 21:44:03 +0900

    mod Entity

--- src/main/java/com/fdel/repository/MenuRepository.java
+++ src/main/java/com/fdel/repository/MenuRepository.java
@@ -1,7 +1,6 @@
 package com.fdel.repository;
 
 import com.fdel.entity.Menu;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
