42505bfdbdd05f792920a2ba2e00a0ef8c3d679a
wookieOH <ehddnrdh@gmail.com>
2022-05-05 21:43:46 +0900

    mod Entity

--- src/main/java/com/fdel/repository/MenuRepository.java
+++ src/main/java/com/fdel/repository/MenuRepository.java
@@ -5,6 +5,4 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
-
-  Optional<Menu> findById(String id);
 }
