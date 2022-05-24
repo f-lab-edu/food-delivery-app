44e9b227538682166fd75eb72e1961e10246b1b1
wookieOH <ehddnrdh@gmail.com>
2022-05-24 15:49:02 +0900

    add order controller

---
+++ src/main/java/com/fdel/controller/OrderController.java
@@ -0,0 +1,35 @@
+package com.fdel.controller;
+
+import com.fdel.service.OrderService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/oders")
+@RequiredArgsConstructor
+@RestController
+public class OrderController {
+
+  private final OrderService orderService;
+
+  @PostMapping
+  public void order(@RequestParam("userId") Long userId,
+      @RequestParam("menuId") Long menuId,
+      @RequestParam("count") int count) {
+
+    orderService.registOrder(userId, menuId, count);
+  }
+
+  @PostMapping("/{orderId}/cancel")
+  public void cancelOrder(@PathVariable("orderId") Long orderId) {
+    orderService.cancelOrder(orderId);
+  }
+
+}
